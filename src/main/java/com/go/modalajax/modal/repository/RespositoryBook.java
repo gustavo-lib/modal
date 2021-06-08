2b2ee3a0154b49a2f2e5255068909d5afde6d7fb
gortiz <ggortiz09@gmail.com>
2021-06-07 23:36:46 -0300

    update repo

---
+++ src/main/java/com/go/modalajax/modal/repository/RespositoryBook.java
@@ -0,0 +1,9 @@
+package com.go.modalajax.modal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.go.modalajax.modal.entity.Book;
+
+public interface RespositoryBook extends JpaRepository<Book, Long> {
+
+}
